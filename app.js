0b763cd424f27689b32c6fedcf1452fbf228163c
Shannon Kroll <skroll912@gmail.com>
2022-08-18 14:15:51 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require(`express`);
+const app = express();
+let port = 3000;
+
+app.use(express.static(`public`)) //hold all of our static resources
+
+app.set(`view engine`, `ejs`)
+
+app.use(require(`./routes/index`))
+app.use(require(`./routes/albums`))
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`);
+})
+
