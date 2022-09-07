227be06c4e1672d33d22168cdffce83f7b0c5bda
Shannon Kroll <skroll912@gmail.com>
2022-09-07 10:46:27 -0700

    working to get the heroku server to run

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const socket = require('socket.io');
 //identify the port
 // let port = 3000;
 //this port allows netlify to connect to this project:
-const PORT = process.env.PORT || 8080 
+const port = process.env.port || 8080 
 
 const router = require("./routes/albums");
 
